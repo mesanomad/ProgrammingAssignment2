0e976b791ef95559e31d0975b59c2d4685cd17ea
mesanomad <mesanomad@gmail.com>
2015-06-21 13:49:12 -0400

    Trying to commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function lol
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -11,5 +11,5 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' lol
 }
